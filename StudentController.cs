065606b151165f24200370cbe9f752d31df3fb5e
PunamThapa09 <20028361@students.koi.edu.au>
2025-08-10 20:40:54 +1000

    Add files via upload

---
+++ StudentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tutorial6._1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+
+        { 
+            return View();
+
+        
+        }
+    }
+}
